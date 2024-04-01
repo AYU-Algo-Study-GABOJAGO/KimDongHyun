544ba4e37291808c0a41767dcd64c6e6d790b3d0
DDongHyun00 <kd3573@naver.com>
2024-04-01 17:23:13 +0900

    feat : 백준 문제풀이

---
+++ week0/Q10799_boj.java
@@ -0,0 +1,17 @@
+package week0;
+
+import java.util.Stack;
+import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Q10799_boj {
+    static Stack<Integer> stackInt = new Stack<>();
+
+    public static void main(String[] args) throws Exception {
+        InputStreamReader sr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(sr);
+        String N = br.readLine();
+
+    }
+}
---
+++ week0/Q11899_boj.java
@@ -0,0 +1,28 @@
+package week0;
+
+import java.util.Stack;
+import java.util.stream.Stream;
+import java.util.Scanner;
+public class Q11899_boj {
+    static Stack<String> stackStr = new Stack<>();
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N;
+        N = sc.nextInt();
+        for(int i = 0; i < N; i++){
+            if (stackStr.isEmpty())
+            {
+
+            }
+            else if (stackStr.peek() == "("){
+
+            }
+        }
+
+    }
+
+}
+
+
+
