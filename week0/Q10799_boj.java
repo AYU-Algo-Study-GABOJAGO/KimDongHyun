bd9ece430ce9cf2b845462df76acce26a5b71712
DDongHyun00 <kd3573@naver.com>
2024-04-04 02:26:11 +0900

    feat : Q10799, Q11899

--- week0/Q10799_boj.java
+++ week0/Q10799_boj.java
@@ -2,16 +2,30 @@ package week0;
 
 import java.util.Stack;
 import java.util.Scanner;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
 
 public class Q10799_boj {
-    static Stack<Integer> stackInt = new Stack<>();
+   static Stack<String> stackStr = new Stack<>();
+   static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        String str = sc.nextLine();
+        int cnt = 0;
 
-    public static void main(String[] args) throws Exception {
-        InputStreamReader sr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(sr);
-        String N = br.readLine();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '('){
+                stackStr.push("(");
+            }
+            else if (str.charAt(i) == ')') {
+                if (!stackStr.isEmpty() && str.charAt(i-1) == '(') {
+                    stackStr.pop();
+                    cnt += stackStr.size();
+                }
+                else {
+                    stackStr.pop();
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
 
     }
 }
--- week0/Q11899_boj.java
+++ week0/Q11899_boj.java
@@ -1,27 +1,28 @@
 package week0;
 
 import java.util.Stack;
-import java.util.stream.Stream;
 import java.util.Scanner;
 public class Q11899_boj {
     static Stack<String> stackStr = new Stack<>();
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int N;
-        N = sc.nextInt();
-        for(int i = 0; i < N; i++){
-            if (stackStr.isEmpty())
-            {
+        String str = sc.nextLine();
+        int count = 0;
 
+        for(int i = 0; i < str.length(); i++){
+            if (str.charAt(i) == '(') {
+                stackStr.push("(");
             }
-            else if (stackStr.peek() == "("){
-
+            else if (str.charAt(i) == ')') {
+                if (stackStr.isEmpty())
+                    count++;
+                else
+                    stackStr.pop();
             }
         }
-
+        System.out.println(count+stackStr.size());
     }
-
 }
 
 
